af36ab1fb55afe943f401a66bf3c277b84a5ecdd
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2022-12-04 15:28:08 +0100

    Initial commit

---
+++ src/main/java/com/webRestFull/webRestFull/WebRestFullApplication.java
@@ -0,0 +1,12 @@
+package com.webRestFull.webRestFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRestFullApplication.class, args);
+	}
+}
---
+++ src/test/java/com/webRestFull/webRestFull/WebRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webRestFull.webRestFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
